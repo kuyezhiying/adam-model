507cced247254057e00c520d5f30927cfd88faf4
Chao Huang <chahuan@microsoft.com>
2018-07-03 07:23:47 +0000

    Update index

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function printMsg() {
+    console.log("Welcome to use Metagraph models.");
+    console.log("This message is from the adam-model package");
+}
+exports.printMsg = printMsg;
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-exports.printMsg = function() {
+export function printMsg() {
 	console.log("Welcome to use Metagraph models.");
     console.log("This message is from the adam-model package");
 }
